557a7b0b38a4f9fad77d73996022f059475f0ef2
Student <example@example.com>
2019-11-12 15:01:24 -0800

    add to .getTeaser method and call it in main.js

--- src/journal.js
+++ src/journal.js
@@ -1,25 +1,33 @@
 export function Journal(title, entry) {
   this.title = title,
   this.entry = entry,
-  this.numberOfWords = ""
+  this.numberOfWords = "";
 }
 
 Journal.prototype.getLetters = function() {
   var count = [0,0];
   var text = this.entry.split("");
   for (var i = 0; i < text.length; i++) {
-    console.log(text[i]);
     if (text[i].match(/[^aeiouAEIOU\s]/g)) {
-      count[0]++
-      console.log('+1 consonant');
+      count[0]++;
     } else if (text[i].match(/[aeiouAEIOU]/g)) {
       count[1]++;
-      console.log('+1 vowel');
     }
   }
   return count;
-}
-
+};
+ 
 Journal.prototype.getTeaser = function(){
   //return the first sentence of the entry. If the sentence is over 8 words, only display those first 8 words.
-}
+  var text = this.entry.split(".");
+  var output = [];
+  text = text[0];
+  text = text.split(" ");
+  for (var i = 0; i < 8; i++) {
+    if(text[i]) {
+      output.push(text[i]);
+      console.log(output);
+    }
+  }
+  return output.join(" ")
+};
--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
 
 import './styles.css';
 import './bootstrap.css';
-import { Journal } from './journal.js'
+import { Journal } from './journal.js';
 
 $(document).ready(function() {
-  console.log('ready');
   $('form#journalForm').submit(function(event){
-    console.log('submitted');
     event.preventDefault();
     var titleInput = $('input#title').val();
     var entryInput = $('textarea#entry').val();
-    console.log(titleInput);
     var myJournal = new Journal(titleInput, entryInput);
     var output = myJournal.getLetters();
-    console.log(output);
-
+    var teaserOutput = myJournal.getTeaser();
+    console.log(teaserOutput);
   });
 });
